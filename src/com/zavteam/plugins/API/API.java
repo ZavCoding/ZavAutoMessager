ecf561bd171a1261d511396c8e57ea6827d5875a
Zach Sugano <zachoooo@gmail.com>
2013-07-05 15:10:57 -0700

    updated API for correctness

--- src/com/zavteam/plugins/API/API.java
+++ src/com/zavteam/plugins/API/API.java
@@ -8,7 +8,7 @@ public class API {
 	 * 
 	 * @param message The message that will be broadcast
 	 */
-	public static void broadcastMessage(String message) {
+	public static void broadcastRawMessage(String message) {
 		String[] messageArray = new String[1];
 		messageArray[0] = message;
 		MessagesHandler.handleMessage(messageArray, null);
@@ -18,7 +18,30 @@ public class API {
 	 * 
 	 * @param messages An array of messages that will be broadcast
 	 */
+	public static void broadcastRawMessage(String[] messages) {
+		MessagesHandler.handleMessage(messages, null);
+	}
+	
+	/** This function is used to immediately broadcast a message using standard formatting for messages
+	 * 
+	 * @param message The message that will be broadcast
+	 * @deprecated Named incorrectly. Changed to broadcastRawMessage.
+	 */
+	@Deprecated
+	public static void broadcastMessage(String message) {
+		String[] messageArray = new String[1];
+		messageArray[0] = message;
+		MessagesHandler.handleMessage(messageArray, null);
+	}
+
+	/** This function is used to immediately broadcast a message(s) using standard formatting for messages
+	 * 
+	 * @param messages An array of messages that will be broadcast
+	 * @deprecated Named incorrectly. Changed to broadcastRawMessage.
+	 */
+	@Deprecated
 	public static void broadcastMessage(String[] messages) {
 		MessagesHandler.handleMessage(messages, null);
 	}
+	
 }
