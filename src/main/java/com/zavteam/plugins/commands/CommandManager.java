e69f55217cfe5d640559ac0913e443aa8a3e4b7c
Zachoooo <zachoooo@gmail.com>
2014-05-09 08:08:22 -0700

    Updated help command

--- src/main/java/com/zavteam/plugins/commands/CommandManager.java
+++ src/main/java/com/zavteam/plugins/commands/CommandManager.java
@@ -2,6 +2,7 @@ package com.zavteam.plugins.commands;
 
 import com.zavteam.plugins.ZavAutoMessager;
 import com.zavteam.plugins.utils.PluginPM;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import se.ranzdo.bukkit.methodcommand.Arg;
 import se.ranzdo.bukkit.methodcommand.Command;
@@ -32,7 +33,7 @@ public class CommandManager {
             permissions = {"zavautomessager.view", "zavautomessager.*"}
     )
     public void root(CommandSender sender) {
-
+        help(sender, 1);
     }
 
     @Command(
@@ -42,10 +43,33 @@ public class CommandManager {
             permissions = {"zavautomessager.view", "zavautomessager.*"}
     )
     public void help(
-            CommandSender sender
+            CommandSender sender,
+            @Arg(name = "page", verifiers = "min[1]|max[2]", def = "1") int page
     ) {
-
+        sender.sendMessage(ChatColor.GOLD + "========= ZavAutoMessager Help =========");
+        switch (page) {
+            case 1: {
+                sender.sendMessage(ChatColor.GOLD + "1. /automessager reload - Reloads config");
+                sender.sendMessage(ChatColor.GOLD + "2. /automessager toggle - Toggle messaging on and off");
+                sender.sendMessage(ChatColor.GOLD + "3. /automessager add <message> - Adds a message to the list");
+                sender.sendMessage(ChatColor.GOLD + "4. /automessager remove <message number> - Removes message from list");
+                sender.sendMessage(ChatColor.GOLD + "5. /automessager ignore - Toggles ignoring messages");
+                sender.sendMessage(ChatColor.GOLD + "=============== Page 1/3 ===============");
+                break;
+            }
+            case 2: {
+                sender.sendMessage(ChatColor.GOLD + "6. /automessager broadcast <message> - Send a message now");
+                sender.sendMessage(ChatColor.GOLD + "7. /automessager about - Displays info about the Main.plugin");
+                sender.sendMessage(ChatColor.GOLD + "8. /automessage list (page) - Shows a message list");
+                sender.sendMessage(ChatColor.GOLD + "9. /automessager help (page)- Displays this menu");
+                sender.sendMessage(ChatColor.GOLD + "10.");
+                sender.sendMessage(ChatColor.GOLD + "=============== Page 2/3 ===============");
+                break;
+            }
+        }
     }
+
+
 
     @Command(
             identifier = "automessager toggle",
